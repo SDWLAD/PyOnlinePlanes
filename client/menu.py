0e36c6b82f4548d19f0981119f47428a8a7a1ac5
SDWLAD <sidorchukvladislav0@gmail.com>
2025-03-29 19:35:14 +0200

    Add settings button

--- client/menu.py
+++ client/menu.py
@@ -12,6 +12,7 @@ class MainMenu:
         self.play_button = Button(pygame.Rect(buttons_x, 100, 200, 100), "client/assets/buttons/play.png", lambda: self.app.change_scene("main"), self.app.ctx)
         self.shop_button = Button(pygame.Rect(buttons_x, 250, 200, 100), "client/assets/buttons/shop.png", lambda: self.app.change_scene("shop"), self.app.ctx)
         self.exit_button = Button(pygame.Rect(buttons_x, 400, 200, 100), "client/assets/buttons/exit.png", lambda: sys.exit(0), self.app.ctx)
+        self.stgs_button = Button(pygame.Rect(10, 10, 50, 50), "client/assets/buttons/settings.png", lambda: self.app.change_scene("settings"), self.app.ctx)
     
     def update(self):...
 
@@ -19,8 +20,10 @@ class MainMenu:
         self.play_button.render()
         self.shop_button.render()
         self.exit_button.render()
+        self.stgs_button.render()
 
     def check_event(self, event):
         self.play_button.handle_event(event)
         self.shop_button.handle_event(event)
         self.exit_button.handle_event(event)
+        self.stgs_button.handle_event(event)
