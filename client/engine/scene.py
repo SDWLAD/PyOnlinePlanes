4f7ced747562925bdf5c38383859f010d10976d9
SDWLAD <sidorchukvladislav0@gmail.com>
2025-04-03 21:52:36 +0300

    Add pause button

--- client/engine/scene.py
+++ client/engine/scene.py
@@ -1,4 +1,7 @@
 import glm
+import pygame
+
+from ui.button import Button
 from .terrain import Terrain
 from .player import Player
 from .components.transform import Transform
@@ -19,15 +22,20 @@ class Scene:
 
         self.terrain = Terrain(self)
 
+        self.pause_button = Button(pygame.Rect(10, 10, 50, 50), "client/assets/buttons/settings.png", lambda: self.app.change_scene("menu"), self.app.ctx)
         self.camera.target = self.plane.transform.position
     
-    def check_event(self, event):...
+    def check_event(self, event):
+        self.pause_button.handle_event(event)
 
     def update(self):
         self.camera.update()
+
         self.plane.update()
         self.terrain.update()
+        self.pause_button.update()
 
     def render(self):
         self.plane.render()
         self.terrain.render()
+        self.pause_button.render()
